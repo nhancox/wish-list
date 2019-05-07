e3efe676d1baf3903461d4c97fbb4b1fb893299f
Nicholas Hancox <nhancox@users.noreply.github.com>
2019-05-06 23:07:08 -0700

    Redesign front-end
    * Refresh layout and CSS
    * Add interactive loading states on buttons
    * Improve form display and reset logic
    * Use a separated model when editing an item

--- client/modules/items/controllers/items.controller.js
+++ client/modules/items/controllers/items.controller.js
@@ -12,12 +12,17 @@
 		$ctrl.addItem = false;
 		$ctrl.editItem = false;
 		$ctrl.error = false;
+		$ctrl.insertForm = {};
 		$ctrl.insertItem = insertItem;
 		$ctrl.items = items;
+		$ctrl.putForm = {};
 		$ctrl.putItem = putItem;
 		$ctrl.removeItem = removeItem;
+		$ctrl.resetForms = resetForms;
+		$ctrl.saving = false;
 		$ctrl.statusMessage = "";
 		$ctrl.success = false;
+		$ctrl.togglePutForm = togglePutForm;
 
 		function insertItem(isValid) {
 			if (!isValid) {
@@ -26,6 +31,7 @@
 				return;
 			}
 
+			$ctrl.saving = true;
 			return itemsService
 				.insert($ctrl.insertForm)
 				.then((response) => {
@@ -42,6 +48,7 @@
 				return;
 			}
 
+			$ctrl.saving = true;
 			let document = {
 				name: $ctrl.putForm.name,
 				notes: $ctrl.putForm.notes
@@ -54,11 +61,12 @@
 		}
 
 		function removeItem(id) {
+			$ctrl.saving = true;
 			return itemsService
 				.remove(id)
 				.then(() => {
-					let removeIndex = $ctrl.items.findIndex((element) => {
-						return element._id === id;
+					const removeIndex = $ctrl.items.findIndex((item) => {
+						return item._id === id;
 					});
 					$ctrl.items.splice(removeIndex, 1);
 
@@ -67,15 +75,39 @@
 				.catch(onError);
 		}
 
-		function onSuccess() {
-			$ctrl.error = false;
-			$ctrl.success = true;
-			$ctrl.addItem = false;
+		function resetForms() {
 			$ctrl.insertForm = {};
 			$scope.addItemForm.$setPristine();
-			$ctrl.editItem = false;
 			$ctrl.putForm = {};
 			$scope.editItemForm.$setPristine();
+			return;
+		}
+
+		function togglePutForm(item) {
+			$ctrl.editItem =
+				$ctrl.putForm._id !== item._id ? true : !$ctrl.editItem;
+			$ctrl.addItem = false;
+			$ctrl.resetForms();
+			$ctrl.putForm = JSON.parse(JSON.stringify(item));
+			return;
+		}
+
+		function onSuccess() {
+			if ($ctrl.editItem) {
+				const swapIndex = $ctrl.items.findIndex((item) => {
+					return item._id === $ctrl.putForm._id;
+				});
+				$ctrl.items[swapIndex] = JSON.parse(
+					JSON.stringify($ctrl.putForm)
+				);
+			}
+
+			$ctrl.error = false;
+			$ctrl.success = true;
+			$ctrl.saving = false;
+			$ctrl.addItem = false;
+			$ctrl.editItem = false;
+			resetForms();
 			return $timeout(() => {
 				$ctrl.success = false;
 			}, 3000);
@@ -84,6 +116,7 @@
 		function onError(response) {
 			$ctrl.statusMessage = response;
 			$ctrl.error = true;
+			$ctrl.saving = false;
 			return;
 		}
 	}
